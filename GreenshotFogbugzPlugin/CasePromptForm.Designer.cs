13c3726016d4c855bab10f9e07906b75c9d4d384
Rohland de Charmoy <rohland.decharmoy@gmail.com>
2012-05-02 11:16:38 +0200

    Removed control box on case selection form.

--- GreenshotFogbugzPlugin/CasePromptForm.Designer.cs
+++ GreenshotFogbugzPlugin/CasePromptForm.Designer.cs
@@ -73,6 +73,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(417, 97);
+            this.ControlBox = false;
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtCaseId);
             this.Controls.Add(this.lblInstruction);
